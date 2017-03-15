9d679189e4722cf6a5a8902a2d6a29b03bf298d0
brandon cooper, grinil khanna and sandro mijatovic <brandon+grinil+sandro@>
2017-03-15 14:32:02 -0700

    pushing list of bikes searched in array

--- build/js/app.js
+++ build/js/app.js
@@ -9,13 +9,19 @@ function Bike() {
 }
 
 Bike.prototype.getSearched = function(brand, color) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + brand + '&frame_colors=' + color + '&location=IP&distance=10&stolenness=stolen&access_token=' + apiToken)
+  var bikeArray = [];
+  for (var i = 0; i < 100; i++)
+  {
+    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + brand + '&frame_colors=' + color + '&location=IP&distance=10&stolenness=stolen&access_token=' + apiToken)
     .then(function(response) {
-      $('.display-area').text("A " + brand + " with the model " + response.bikes[0].frame_model + " has been found in your area with the frame color " + color + ".");
-
+      // $('#all-bikes').text
+      var oneBike = "A " + brand + " with the model " + response.bikes.frame_model + " has been found in your area with the frame color " + color + "."
+      bikeArray.push(oneBike);
     }).fail(function(error) {
-      $('.display-area').text(error.responseJSON.message);
+      $('.display-area').append(error.responseJSON.message);
     });
+  }
+  console.log(bikeArray);
 };
 
 exports.bikeModule = Bike;
