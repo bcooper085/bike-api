7106864c548a28d8ccb966086fa118aff4126d13
brandon cooper, grinil khanna and sandro mijatovic <brandon+grinil+sandro@>
2017-03-15 16:56:08 -0700

    add changes to bike.js, still unable to append all items in array.

--- js/bike.js
+++ js/bike.js
@@ -5,19 +5,33 @@ function Bike() {
 }
 
 Bike.prototype.getSearched = function(brand, color) {
-  var bikeArray = [];
-  for (var i = 0; i < 100; i++)
-  {
     $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + brand + '&frame_colors=' + color + '&location=IP&distance=10&stolenness=stolen&access_token=' + apiToken)
     .then(function(response) {
       // $('#all-bikes').text
-      var oneBike = "A " + brand + " with the model " + response.bikes.frame_model + " has been found in your area with the frame color " + color + "."
-      bikeArray.push(oneBike);
+      response.bikes.foreach(item); {
+
+        $('#all-bikes').append(item.title + " is the bike title.");
+      }
     }).fail(function(error) {
       $('.display-area').append(error.responseJSON.message);
     });
-  }
-  console.log(bikeArray);
 };
 
 exports.bikeModule = Bike;
+
+
+// var searchArrayReq = $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + brand + '&frame_colors=' + color + '&location=IP&distance=10&stolenness=stolen&access_token=' + apiToken);
+//
+//
+// Bike.prototype.getSearched = function(brand, color) {
+//
+// searchArrayReq.foreach(item in )
+// {
+//   return item.then(function(response) {
+//     $(".display-area").text("A " + brand + " with the model " + response.bikes.frame_model + " has been found in your area with the frame color " + color + ".");
+//   }).fail(function(error) {
+//     $('.display-area').text(error.responseJSON.message);
+//   });
+// };
+//
+// };
